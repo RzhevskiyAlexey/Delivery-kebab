74bdf6dc479fed7770bbeccd08d81eb88c651ad6
Emin <eminov9898@mail.ru>
2023-04-21 11:44:43 +0300

    auth1

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 export default function HomePage() {
   return (
-    <div className='container mt-5 d-flex justify-content-center'>
-      <h1>Welcome to Delivery-Kebab</h1>
+    <div className='container mt-5' style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1 style={{ width: '100%', fontSize: '5rem', fontFamily: 'cursive', fontWeight: 'bold', textTransform: 'uppercase', color: 'black', whiteSpace: 'nowrap', marginBottom: '2rem', animation: 'slide-left 10s linear infinite' }}>Welcome to Delivery-Kebab</h1>
+      <style>
+        {`
+          @keyframes slide-left {
+            from {
+              transform: translateX(100%);
+            }
+            to {
+              transform: translateX(-100%);
+            }
+          }
+        `}
+      </style>
     </div>
   );
 }
+
+
