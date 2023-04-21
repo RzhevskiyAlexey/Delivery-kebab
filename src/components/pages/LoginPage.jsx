eba7c777c21477cb51d3b7f94e6deb9e0bba00e1
Alexey <alexey.rzhevskiy@gmail.com>
2023-04-21 14:48:53 +0300

    final one

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -6,10 +6,11 @@ export default function LoginPage() {
   const submitHandler = async (event) => {
     event.preventDefault();
     const formData = Object.fromEntries(new FormData(event.target));
+    console.log('==========> ',formData);
     try {
       const response = await axios.post('/api/auth/login', formData);
       if (response.status === 200) {
-        window.location = '/drinks';
+        window.location = '/';
       }
     } catch (error) {
       console.log(error);
@@ -63,57 +64,3 @@ export default function LoginPage() {
   );
 }
 
-// import React from 'react';
-// import axios from 'axios';
-
-// export default function LoginPage() {
-//   const submitHandler = async (event) => {
-//     event.preventDefault();
-//     const formData = Object.fromEntries(new FormData(event.target));
-//     try {
-//       const response = await axios.post('/api/auth/login', formData);
-//       if (response.status === 200) {
-//         window.location = '/drinks';
-//       }
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-
-//   return (
-//     <div
-//       style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh' }}
-//     >
-//       <form onSubmit={submitHandler}>
-//         <h1>Авторизация</h1>
-//         <br />
-//         <div className="mb-3">
-//           <label htmlFor="exampleInputEmail1" className="form-label">
-//             Email address
-//             <input
-//               name="email"
-//               type="email"
-//               className="form-control"
-//               id="exampleInputEmail1"
-//               aria-describedby="emailHelp"
-//             />
-//           </label>
-//         </div>
-//         <div className="mb-3">
-//           <label htmlFor="exampleInputPassword1" className="form-label">
-//             Password
-//             <input
-//               name="password"
-//               type="password"
-//               className="form-control"
-//               id="exampleInputPassword1"
-//             />
-//           </label>
-//         </div>
-//         <button type="submit" className="btn btn-primary">
-//           Submit
-//         </button>
-//       </form>
-//     </div>
-//   );
-// }
